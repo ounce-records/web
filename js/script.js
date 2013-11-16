de781e6ff9983a66a60a826d7769bd3e6f6561a2
Tom Allen <tomjamesallenweb@gmail.com>
2013-11-16 12:03:44 +0000

    Added parallax effect to header images. Style tweaks. Js structure tweaks. '_blank' added to form to open submission in new tab.

--- js/script.js
+++ js/script.js
@@ -3,42 +3,46 @@ var ounce_js = {};
 ounce_js.resize_homepage_img = (function(){
 	function init(){
 		if($('.full-height-header-wrapper').length){
-			var $container = $('.full-height-header-wrapper');
-			var container_height = $container.outerHeight(true);
-			var container_width = $container.outerWidth(true);
-			var container_aspect_ratio = container_height/container_width;
-
-			var $image = $('.header-img');
-			var image_height = $image.outerHeight(true);
-			var image_width = $image.outerWidth(true);
-			var image_aspect_ratio = image_height/image_width;
-
-			var left_pos;
-			var top_pos;
-
-			// TODO: vertically center image
-
-			if(container_aspect_ratio>image_aspect_ratio){
-				$image.addClass('match-height');
-				image_width = $image.outerWidth(true);
-				left_pos = (container_width-image_width)/2;
-				$image.css('left', left_pos+'px');
-				top_pos = 0;
-				$image.css('top', top_pos+'px');
-			}else{
-				$image.removeClass('match-height');
-				image_height = $image.outerHeight(true);
-				left_pos = 0;
-				$image.css('left', left_pos+'px');
-				top_pos = (container_height-image_height)/2;
-				$image.css('top', top_pos+'px');
-			}
+			resize();
+			bind_events();
 		}
-		bind_events();
+	}
+	function resize(){
+		var $container = $('.full-height-header-wrapper');
+		var container_height = $container.outerHeight(true);
+		var container_width = $container.outerWidth(true);
+		var container_aspect_ratio = container_height/container_width;
+
+		var $image = $('.header-img');
+		var image_height = $image.outerHeight(true);
+		var image_width = $image.outerWidth(true);
+		var image_aspect_ratio = image_height/image_width;
+
+		var left_pos;
+		var top_pos;
+
+		if(container_aspect_ratio>image_aspect_ratio){
+			$image.addClass('match-height');
+			image_width = $image.outerWidth(true);
+			left_pos = (container_width-image_width)/2;
+			$image.css('left', left_pos+'px');
+			top_pos = 0;
+			$image.css('top', top_pos+'px');
+		}else{
+			$image.removeClass('match-height');
+			image_height = $image.outerHeight(true);
+			left_pos = 0;
+			$image.css('left', left_pos+'px');
+			top_pos = (container_height-image_height)/2;
+			$image.css('top', top_pos+'px');
+		}
 	}
 	function bind_events(){
 		$('.header-img').load(function() {
 			ounce_js.resize_homepage_img.init();
+		});
+		$(window).resize(function() {
+			resize();
 		});
 	}
 	return {
@@ -47,11 +51,32 @@ ounce_js.resize_homepage_img = (function(){
 })();
 
 ounce_js.parallax = (function(){
+	var breakpoint;
+	function init(set_breakpoint){
+		breakpoint = set_breakpoint;
+		position_image();
+		bind_events();
+	}
 	function position_image(){
-		
+		var window_width = $(window).width();
+		var $header_img = $('.header .header-img');
+		if(window_width > breakpoint){
+			var scroll_pos = $(window).scrollTop();
+			$header_img.css('margin-top', (scroll_pos/10)+'px');
+		}else{
+			$header_img.css('margin-top', '0px');
+		}	
+	}
+	function bind_events(){
+		$(window).scroll(function(){
+			position_image();
+		}).trigger("scroll");
+		$(window).resize(function() {
+			position_image();
+		});
 	}
 	return {
-		position_image: position_image
+		init: init,
 	};
 })();
 
@@ -65,7 +90,6 @@ ounce_js.map_resize = (function(){
 		bind_events();
 	}
 	function resize_map(){
-		console.log('resize_map');
 		var $map = $('#map-canvas');
 		var window_height = $(window).height();
 		$map.height(window_height * 0.5);
@@ -76,7 +100,7 @@ ounce_js.map_resize = (function(){
 		});
 	}
 
-	return{
+	return {
 		init: init
 	}
 })();
@@ -85,12 +109,5 @@ ounce_js.map_resize = (function(){
 $(document).ready(function(){
 	ounce_js.resize_homepage_img.init();
 	ounce_js.map_resize.init();
-});
-
-$(window).resize(function() {
-	ounce_js.resize_homepage_img.init();
-});
-
-$(window).scroll(function(){
-  ounce_js.parallax;
+	ounce_js.parallax.init(700);
 });
